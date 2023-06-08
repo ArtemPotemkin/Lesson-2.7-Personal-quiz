3ab67ee350667f7bb440a4a0bb834562b2bf1e5f
Artem Potemkin <art.potemkin@gmail.com>
2023-06-09 01:08:49 +0300

    Lesson 2.7 part 1

---
+++ Lesson 2.7 Personal quiz/Models/Question.swift
@@ -0,0 +1,78 @@
+//
+//  Question.swift
+//  Lesson 2.7 Personal quiz
+//
+//  Created by Артём Потёмкин on 09.06.2023.
+//
+
+struct Question {
+    let title: String
+    let responseType: ResponseType
+    let answers: [Answer]
+    
+    static func getQuestions() -> [Question] {
+        [
+            Question(
+                title: "Какую пишу вы предпочитаете",
+                responseType: .single,
+                answers: [
+                    Answer(title: "Стейк", animal: .dog),
+                    Answer(title: "Рыба", animal: .cat),
+                    Answer(title: "Морковь", animal: .rabbit),
+                    Answer(title: "Кукуруза", animal: .turtle)
+                ]
+            ),
+            Question(
+                title: "Что вам нравится больше",
+                responseType: .multiple,
+                answers: [
+                    Answer(title: "Плавать", animal: .dog),
+                    Answer(title: "Спать", animal: .cat),
+                    Answer(title: "Обниматься", animal: .rabbit),
+                    Answer(title: "Есть", animal: .turtle)
+                ]
+            ),
+            Question(
+                title: "Любите ли вы поездки на машине?",
+                responseType: .ranged,
+                answers: [
+                    Answer(title: "Ненавижу", animal: .cat),
+                    Answer(title: "Нервничаю", animal: .rabbit),
+                    Answer(title: "Не замечаю", animal: .turtle),
+                    Answer(title: "Обожая", animal: .dog)
+                ]
+            )
+        ]
+    }
+}
+
+enum ResponseType {
+    case single
+    case multiple
+    case ranged
+}
+
+struct Answer {
+    let title: String
+    let animal: Animal
+}
+
+enum Animal: Character {
+    case dog = "🐶"
+    case cat = "🐱"
+    case rabbit = "🐰"
+    case turtle = "🐢"
+    
+    var definition: String {
+        switch self {
+        case .dog:
+            return "Вы собака. Гав-гав-гав! Или тяв-тяв, если вы маленькая собака. Ухх, какая собака!"
+        case .cat:
+            return "Вы кошка. Мяу-мяу, мур-мур и все такое."
+        case .rabbit:
+            return "Вы заяц или кролик, я уже забыл. Держи морковку 🥕"
+        case .turtle:
+            return "Вы черепаха. 5 км/ч для вас это Фернандо Алонсо на Ф1!"
+        }
+    }
+}
--- Lesson 2.7 Personal quiz/ViewControllers/ViewController.swift
+++ Lesson 2.7 Personal quiz/ViewControllers/ViewController.swift
@@ -8,10 +8,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    private let questions = Question.getQuestions()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
     }
 
 
