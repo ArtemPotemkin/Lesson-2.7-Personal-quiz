eeca61bb62944fa1b00019bd8f68f99592fd1510
Artem Potemkin <art.potemkin@gmail.com>
2023-06-08 23:22:25 +0300

    Initial Commit

---
+++ Lesson 2.7 Personal quiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson 2.7 Personal quiz
+//
+//  Created by Артём Потёмкин on 08.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
