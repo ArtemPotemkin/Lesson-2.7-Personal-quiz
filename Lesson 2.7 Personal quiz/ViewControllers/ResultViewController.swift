2ec7211d5d3516fdabfbbff7a521f006e53fb87c
Artem Potemkin <art.potemkin@gmail.com>
2023-06-14 23:50:01 +0300

    logic - ok, calculate - ok. Ready to codeReview =)

--- Lesson 2.7 Personal quiz/ViewControllers/ResultViewController.swift
+++ Lesson 2.7 Personal quiz/ViewControllers/ResultViewController.swift
@@ -13,14 +13,17 @@ class ResultViewController: UIViewController {
     var resultAnimals: [Animal] {
         resultAnswers.map {$0.animal}
     }
+    
     var animalsCount: [Animal : Int] = [:]
     
+    @IBOutlet var resultLabel: UILabel!
     @IBOutlet var definitionLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.setHidesBackButton(true, animated: true)
-        
+        resultLabel.text = "Вы - \(calculateResult().rawValue)"
+        definitionLabel.text = calculateResult().definition
         
     }
     
@@ -31,13 +34,17 @@ class ResultViewController: UIViewController {
         //dismiss(animated: true)
     }
     
+    private func calculateResult() -> Animal {
+        resultAnimals.forEach {
+            animalsCount[$0, default: 0] += 1
+        }
+        let sortedAnimals = animalsCount.sorted { $0.value > $1.value}
+        return sortedAnimals[0].key
+    }
     
     deinit {
         print("\(type(of: self)) has been deallocated")
     }
 }
 
-extension ResultViewController {
-    
-    
-}
+
